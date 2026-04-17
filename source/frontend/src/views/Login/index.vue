cc2f189c730de385aea4c38101090bc80e0154ce
wujiulong <wujiulong@anosi.cn>
2026-04-17 13:19:00 +0800

    登录及代理调整

--- source/frontend/src/views/Login/index.vue
+++ source/frontend/src/views/Login/index.vue
@@ -74,8 +74,8 @@ const loginFormRef = ref<FormInstance>()
 const loading = ref(false)
 
 const loginForm = reactive({
-  username: 'admin',
-  password: 'admin123'
+  username: '',
+  password: ''
 })
 
 const loginRules: FormRules = {
--- source/frontend/src/views/Mobile/Login/index.vue
+++ source/frontend/src/views/Mobile/Login/index.vue
@@ -92,8 +92,8 @@ const userStore = useUserStore()
 const loading = ref(false)
 
 const loginForm = reactive({
-  username: 'admin',
-  password: 'admin123'
+  username: '',
+  password: ''
 })
 
 // 钉钉登录
